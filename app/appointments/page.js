8c5945d65b52a8a6089e959eac68372035ad840a
Sana Muhammad Ashraf <ashras17@mcmaster.ca>
2024-08-22 12:26:40 -0400

    chatbot

---
+++ app/appointments/page.js
@@ -0,0 +1,12 @@
+"use client";
+import { Box, Typography } from "@mui/material";
+import NavBar from "../components/NavBar";
+
+export default function Appointments() {
+return(
+    <Box>
+    < NavBar />
+    <Typography variant="h6">Coming Soon!</Typography>
+    </Box>
+)
+}
